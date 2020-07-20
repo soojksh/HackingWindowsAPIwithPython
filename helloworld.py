ad2be8f81e4b9a560a2e762a41ad4d5eeaade418
Sooj Ksh <63094180+soojksh@users.noreply.github.com>
2020-07-20 21:51:38 +0545

    Update helloworld.py

--- helloworld.py
+++ helloworld.py
@@ -1,24 +1,40 @@
+#Hello World Windows API Style.
+
+# Import the required module to handle Windows API Calls
 import ctypes
 
+# Grab a handle to User32.dll & kernel32.dll
 user_handle = ctypes.WinDLL("User32.dll")
 k_handle = ctypes.WinDLL("Kernel32.dll")
 
+# Win API Call
+# int MessageBoxA(
+# HWND hWnd,
+# LPCTSTR lpText,
+# LPCTSTR lpCaption,
+# UINT, uType
+# );
+
+# Setting Up The Params
 hWnd = None
 lpText = "Hello World"
-lpCaption = "Hello Students"
+lpCaption = "Hello Friend"
 uType = 0x00000001
 
+# Calling the Windows API Call
 response = user_handle.MessageBoxW(hWnd, lpText, lpCaption, uType)
 
+#Check the Errors.
 error = k_handle.GetLastError()
 if error != 0:
     print("Error Code: (0)".format(error))
     exit(1)
     
+#Check what the User Clicked.
 if response == 1:
     print("User Clicked OK!")
     
 elif response == 2:
-    print("User Clicked Cancel!")
+    print("User Exited!")
 
 
